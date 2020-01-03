fa137f935e59e8316d4f48169aac360f776a83db
윤수영 <gsdaisy@helixtech.co.kr>
2020-01-03 15:59:22 +0900

    Remote Service

---
+++ app/src/main/java/com/example/myapplication/RemoteService.java
@@ -0,0 +1,82 @@
+package com.example.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+
+public class RemoteService extends Service {
+    private final String TAG = "RemoteService";
+    public static final int MSG_CLIENT_CONNECT = 1;
+    public static final int MSG_CLIENT_DISCONNECT = 2;
+    public static final int MSG_ADD_VALUE = 3;
+    public static final int MSG_ADDED_VALUE = 4;
+
+    private ArrayList<Messenger> mClientCallbacks = new ArrayList<>();
+    final Messenger mMessenger = new Messenger(new CallbackHandler());
+    int mValue = 0;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private class CallbackHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case MSG_CLIENT_CONNECT:
+                    Log.d(TAG, "Reveived MSG_CLIENT_CONNECT message from client");
+                    mClientCallbacks.add(msg.replyTo);
+                    break;
+                case MSG_CLIENT_DISCONNECT:
+                    Log.d(TAG, "Reveived MSG_CLIENT_DISCONNECT message from client");
+                    mClientCallbacks.remove(msg.replyTo);
+                    break;
+                case MSG_ADD_VALUE:
+                    Log.d(TAG, "Reveived message from client : MSG_ADD_VALUE");
+                    mValue += msg.arg1;
+                    for (int i = mClientCallbacks.size() - 1; i >= 0; i--) {
+                        try {
+                            Log.d(TAG, "Send msg_added_value mnessage to client");
+                            Message added_msg = Message.obtain(null, RemoteService.MSG_ADDED_VALUE);
+                            added_msg.arg1 = mValue;
+                            mClientCallbacks.get(i).send(added_msg);
+                        } catch (RemoteException e) {
+                            mClientCallbacks.remove(i);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
