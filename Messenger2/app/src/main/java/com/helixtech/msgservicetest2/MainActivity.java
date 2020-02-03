98c981c2862b9705322d1cd773957e4cc68ea9a8
GsDaisy <dbstndud0422@naver.com>
2020-02-03 11:11:09 +0900

    [Modify] Subtract client's MainActivity

--- Messenger2/app/src/main/java/com/helixtech/msgservicetest2/MainActivity.java
+++ Messenger2/app/src/main/java/com/helixtech/msgservicetest2/MainActivity.java
@@ -1,23 +1,11 @@
 package com.helixtech.msgservicetest2;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+/*
     private Messenger mService;
     private final Messenger mMessenger = new Messenger(new IncomingHandler());
     private static final String TAG = "MainActivity";
@@ -60,23 +48,24 @@ public class MainActivity extends AppCompatActivity {
                 Message msg = Message.obtain(null, MSG_REGISTER_CLIENT);
                 msg.replyTo = mMessenger;
                 mService.send(msg);
-                Log.d(TAG, "Msg set value 전 : "+this.hashCode());
+                Log.d(TAG, "Msg set value 전 : " + this.hashCode());
                 msg = Message.obtain(null, MSG_SET_VALUE, this.hashCode(), 0);
                 mService.send(msg);
-                Log.d(TAG, "Msg set value 후 : "+msg.arg1);
+                Log.d(TAG, "Msg set value 후 : " + msg.arg1);
                 mIsBound = true;
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     };
+*/
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        LinearLayout ll = new LinearLayout(getBaseContext());
+        setContentView(R.layout.activity_main);
+/*        LinearLayout ll = new LinearLayout(getBaseContext());
         ll.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
         ll.setOrientation(LinearLayout.VERTICAL);
 
@@ -115,16 +104,16 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         ll.addView(btn3, param);
         ll.addView(btn4, param);
-        setContentView(ll);
+        setContentView(ll);*/
     }
 
-    @Override
+    /*@Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mIsBound){
+        if (mIsBound) {
             unbindService(conn);
             mIsBound = false;
         }
 
-    }
+    }*/
 }
